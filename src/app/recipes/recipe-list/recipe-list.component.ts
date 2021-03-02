4960d567ee82a1d130b0c62606e4cf570863e6cf
Sone <sthaya@gmail.com>
2021-03-01 16:45:27 -0800

    set up recipe list to map through array and display recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'this is a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'this is a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
   ];
 
   constructor() {}
