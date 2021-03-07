b9ea15ff54a0f39e54979ac2a07393365978a7b7
Sone <sthaya@gmail.com>
2021-03-06 20:08:47 -0800

    setup form to switch between login and sign up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-
 export class AuthComponent {
+  isLoginMode = true;
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
