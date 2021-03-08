3293cc38d4032ba9d3f82d1ca110e7dffe9eac7a
Sone <sthaya@gmail.com>
2021-03-08 11:38:06 -0800

    created shopping list actions and setup store in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LoggingService } from './logging.service';
+import { StoreModule } from '@ngrx/store';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,12 +7,14 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     HttpClientModule,
     SharedModule,
     CoreModule,
