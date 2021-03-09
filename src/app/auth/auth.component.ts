722db0fc3d164d1df40cc8319ee5b98127d970f5
Sone <sthaya@gmail.com>
2021-03-08 21:06:38 -0800

    added sign up and refactored code

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -40,7 +40,7 @@ export class AuthComponent implements OnInit, OnDestroy {
       this.isLoading = authState.loading;
       this.error = authState.authError;
       if (this.error) {
-        this.showErrorAlert(this.error)
+        this.showErrorAlert(this.error);
       }
     });
   }
@@ -56,17 +56,15 @@ export class AuthComponent implements OnInit, OnDestroy {
     const email = form.value.email;
     const password = form.value.password;
 
-    let authObs: Observable<AuthResponseData>;
-
-    this.isLoading = true;
-
     if (this.isLoginMode) {
       //authObs = this.authService.login(email, password);
       this.store.dispatch(
         new AuthActions.LoginStart({ email: email, password: password })
       );
     } else {
-      authObs = this.authService.signup(email, password);
+      this.store.dispatch(
+        new AuthActions.SignupStart({ email: email, password: password })
+      );
     }
 
     // authObs.subscribe(
